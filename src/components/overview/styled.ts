a893a880f81d231a67f65a4a04aec4e271538f8e
muhrusdi <mhmrofficial@gmail.com>
2022-03-20 03:43:43 +0800

    update overview

--- src/components/overview/styled.ts
+++ src/components/overview/styled.ts
@@ -1,3 +1,25 @@
 import { styled } from "@/styled"
+import img from "@/images/bg-overview.png"
 
-export const StyledOverview = styled("div", {})
+export const StyledOverview = styled("div", {
+  position: "relative",
+  "&:after": {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    content: "' '",
+    backgroundSize: "cover",
+    zIndex: 1,
+    backgroundImage: `url(${img})`
+  }
+})
+
+export const StyledSpan = styled("span", {
+  background: "linear-gradient(180deg, #CF7AD0 0%, #701A72 100%)",
+  "-webkit-background-clip": "text",
+  "-webkit-text-fill-color": "transparent",
+  backgroundClip: "text",
+  textFillColor: "transparent"
+})
