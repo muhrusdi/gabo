5e0fa4d9f5b4fb3344a5c6def6a02eb47d18c8f5
muhrusdi <mhmrofficial@gmail.com>
2022-03-20 15:50:44 +0800

    add apollo graphql

---
+++ src/apollo/client.ts
@@ -0,0 +1,14 @@
+import { ApolloClient, InMemoryCache, from, HttpLink } from "@apollo/client"
+import fetch from "isomorphic-fetch"
+
+const httpLink = new HttpLink({
+  uri: process.env.GATSBY_API_URL,
+  fetch,
+})
+
+const link = from([httpLink])
+
+export const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache(),
+})
--- src/containers/wrap-elements/wrap-elements.tsx
+++ src/containers/wrap-elements/wrap-elements.tsx
@@ -1,21 +1,20 @@
 import React from "react"
-// import { ApolloProvider } from "@apollo/client"
-// import { client } from "../../apollo/client"
-// import { Layout } from "@/containers/layout"
+import { ApolloProvider } from "@apollo/client"
+import { client } from "../../apollo/client"
 import { getCssText } from "@/styled"
 
-// type Props = {
-//   element: React.ReactNode
-//   props: any
-// }
+type Props = {
+  element: React.ReactNode
+  props: any
+}
 
 type RenderBodyProps = {
   setHeadComponents?: any
 }
 
-// export const wrapRootElement: React.FC<Props> = ({ element }) => (
-//   <ApolloProvider client={client}>{element}</ApolloProvider>
-// )
+export const wrapRootElement: React.FC<Props> = ({ element }) => (
+  <ApolloProvider client={client}>{element}</ApolloProvider>
+)
 
 // export const wrapPageElement: React.FC<Props> = ({ element, props }) => (
 //   <Layout {...props}>{element}</Layout>
