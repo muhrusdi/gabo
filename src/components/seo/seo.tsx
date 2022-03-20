684b89f458dd06e513ed4c7b7160c2d556ca6b0e
muhrusdi <mhmrofficial@gmail.com>
2022-03-20 12:55:31 +0800

    add component seo

---
+++ src/components/seo/seo.tsx
@@ -0,0 +1,49 @@
+import React from "react"
+import { Helmet } from "react-helmet"
+import { useStaticQuery, graphql } from "gatsby"
+
+type Props = {
+  title: string
+  description?: string
+  lang?: string
+}
+
+export const SEO: React.FC<Props> = ({title, description, lang}) => {
+  const { site } = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+            description
+            author
+            keywords
+            siteUrl
+          }
+        }
+      }
+    `
+  )
+
+  const metaDescription = description || site.siteMetadata.description
+
+  return (
+    <Helmet
+      htmlAttributes={{
+        lang,
+      }}
+      meta={[
+        {
+          name: `description`,
+          content: metaDescription,
+        },
+        {
+          name: "keywords",
+          content: site.siteMetadata.keywords.join(","),
+        },
+      ]}
+    >
+      <title>{title}</title>
+    </Helmet>
+  )
+}
